ae9f1d103ec7530c5d330028dfaa46178d4ef286
Hector Peeters <hector.peeters@gmail.com>
2022-05-05 15:32:28 +0200

    Setup project structure

--- src/Agda/Compiler/ToRust.hs
+++ src/Agda/Compiler/ToRust.hs
@@ -1,1 +1,32 @@
 module Agda.Compiler.ToRust where
+
+import Agda.TypeChecking.Monad (Definition, TCM)
+import Control.DeepSeq (NFData)
+import Control.Monad.Reader (ReaderT (runReaderT))
+import Control.Monad.State (StateT, evalStateT)
+import GHC.Generics (Generic)
+
+data RustOptions = RustOptions {} deriving (Generic, NFData)
+
+data RustStatement = RustStatement {}
+
+data ToRustState = ToRustState {}
+
+data ToRustEnv = ToRustEnv {toRustOptions :: RustOptions}
+
+type ToRustM a = StateT ToRustState (ReaderT ToRustEnv TCM) a
+
+initToRustEnv :: RustOptions -> ToRustEnv
+initToRustEnv opts = ToRustEnv opts
+
+initToRustState :: ToRustState
+initToRustState = ToRustState {}
+
+runToRustM :: RustOptions -> ToRustM a -> TCM a
+runToRustM opts = (`runReaderT` initToRustEnv opts) . (`evalStateT` initToRustState)
+
+class ToRust a b where
+  toRust :: a -> ToRustM b
+
+instance ToRust Definition (Maybe RustStatement) where
+  toRust _ = error "Not implemented"
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,49 @@
 module Main where
 
+import Agda.Compiler.Backend
+import Agda.Compiler.Common
+import Agda.Compiler.ToRust (RustOptions (RustOptions), RustStatement, ToRust (toRust), runToRustM)
+import Agda.Interaction.Options (OptDescr)
+import Agda.Main (runAgda)
+import Agda.Utils.Pretty (prettyShow)
+import Control.Monad.Reader (MonadIO (liftIO))
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Prelude hiding (empty, null)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runAgda [backend]
+
+backend :: Backend
+backend = Backend backend'
+
+backend' :: Backend' RustOptions RustOptions () () (Maybe RustStatement)
+backend' =
+  Backend'
+    { backendName = "agda2rust",
+      options = RustOptions,
+      commandLineFlags = rustFlags,
+      isEnabled = const True,
+      preCompile = rustPreCompile,
+      postCompile = \_ _ _ -> return (),
+      preModule = \_ _ _ _ -> return $ Recompile (),
+      compileDef = rustCompileDef,
+      postModule = rustPostModule,
+      backendVersion = Nothing,
+      scopeCheckingSuffices = False,
+      mayEraseType = \_ -> return True
+    }
+
+rustFlags :: [OptDescr (Flag RustOptions)]
+rustFlags = []
+
+rustPreCompile :: RustOptions -> TCM RustOptions
+rustPreCompile = return
+
+rustCompileDef :: RustOptions -> () -> IsMain -> Definition -> TCM (Maybe RustStatement)
+rustCompileDef opts _ isMain def = runToRustM opts $ toRust def
+
+rustPostModule :: RustOptions -> () -> IsMain -> ModuleName -> [Maybe RustStatement] -> TCM ()
+rustPostModule opts _ isMain modName defs = do
+  let fileName = prettyShow (last $ mnameToList modName) ++ ".rs"
+  liftIO $ T.writeFile fileName ""
